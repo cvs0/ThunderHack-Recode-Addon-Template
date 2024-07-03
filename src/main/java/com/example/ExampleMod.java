6d9f86450eee522ab61c6b44be42b21bb21beaa2
cvs0 <143862758+cvs0@users.noreply.github.com>
2024-07-03 15:18:16 -0600

    fix: Updated the addon template for the newest thunderhack version.

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -5,8 +5,11 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import thunder.hack.api.IAddon;
+import thunder.hack.cmd.Command;
+import thunder.hack.cmd.impl.ExampleCommand;
 import thunder.hack.modules.Module;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,5 +25,30 @@ public class ExampleMod implements IAddon {
 	public List<Module> getModules() {
 		// Return a list of your modules here
 		return Arrays.asList(new ExampleModule());
+	}
+
+	@Override
+	public List<Command> getCommands() {
+		return null;
+	}
+
+	@Override
+	public String getPackage() {
+		return "com.example";
+	}
+
+	@Override
+	public String getName() {
+		return "ExampleMod";
+	}
+
+	@Override
+	public String getAuthor() {
+		return "cvs0";
+	}
+
+	@Override
+	public String getRepo() {
+		return "https://github.com/cvs0/ThunderHack-Recode-Addon-Template";
 	}
 }
