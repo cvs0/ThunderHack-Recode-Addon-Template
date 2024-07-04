e12b6d3308ebc0f62a515a21c475ffaf415cf297
cvs0 <143862758+cvs0@users.noreply.github.com>
2024-07-03 19:14:41 -0600

    fix: Fixed command issue

---
+++ src/main/java/com/example/commands/ExampleCommand.java
@@ -0,0 +1,29 @@
+package com.example.commands;
+
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import net.minecraft.command.CommandSource;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import org.jetbrains.annotations.NotNull;
+import thunder.hack.ThunderHack;
+import thunder.hack.cmd.Command;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
+
+public abstract class ExampleCommand extends Command {
+    public ExampleCommand() {
+        super("help");
+    }
+
+    @Override
+    public void executeBuild(@NotNull LiteralArgumentBuilder<CommandSource> builder) {
+        builder.executes(context -> {
+            sendMessage("Commands: \n");
+
+            return SINGLE_SUCCESS;
+        });
+    }
+}
